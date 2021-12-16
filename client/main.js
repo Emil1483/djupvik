7d072057db7be5f9e491cbb4f69816c294c7ff81
Emil Djupvik <emil14833@gmail.com>
2021-12-16 20:12:28 +0100

    add cum button

--- client/main.js
+++ client/main.js
@@ -1,7 +1,9 @@
+const xhr = new XMLHttpRequest();
+
 const liveImageDiv = document.getElementById('live-image')
 const liveImageDivTemp = document.getElementById('live-image-temp')
 
-const APP_URL = window.location.hostname === 'localhost' ? 'http://localhost:1881/live' : 'https://server.djupvik.dev/live';
+const APP_URL = window.location.hostname === 'localhost' ? 'http://localhost:1881/' : 'https://server.djupvik.dev/';
 
 const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
@@ -11,13 +13,25 @@ function image(url) {
     return img
 }
 
+function cum() {
+    xhr.open('POST', APP_URL + 'notify')
+    
+    xhr.onreadystatechange = () => {
+        if (xhr.readyState !== 4) return
+
+        alert(xhr.responseText)
+    }
+
+    xhr.send()
+}
+
 const show = (element) => element.style.display = 'block'
 const hide = (element) => element.style.display = 'none'
 
 hide(liveImageDivTemp)
 
 const updateImg = (async function () {
-    const response = await fetch(APP_URL + '?=' + new Date().getTime())
+    const response = await fetch(APP_URL + 'live?=' + new Date().getTime())
     const blob = await response.blob()
 
     const url = URL.createObjectURL(blob)
