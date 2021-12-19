ee9dd771586331a38b6f1c64b7fe37950c7f3049
Emil Djupvik <emil14833@gmail.com>
2021-12-19 03:47:08 +0100

    use old method

--- client/main.js
+++ client/main.js
@@ -1,10 +1,44 @@
 const xhr = new XMLHttpRequest();
 
-const liveVideo = document.getElementById('live-video')
+const liveImageDiv = document.getElementById('live-image')
+const liveImageDivTemp = document.getElementById('live-image-temp')
 
 const APP_URL = window.location.hostname === 'localhost' ? 'http://localhost:1881/' : 'https://server.djupvik.dev/';
 
-liveVideo.setAttribute('src', APP_URL + 'live')
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+function image(url) {
+    const img = new Image()
+    img.src = url
+    return img
+}
+
+const show = (element) => element.style.display = 'block'
+const hide = (element) => element.style.display = 'none'
+
+hide(liveImageDivTemp)
+
+const updateImg = (async function () {
+    const response = await fetch(APP_URL + 'frame?=' + new Date().getTime())
+    const blob = await response.blob()
+
+    const url = URL.createObjectURL(blob)
+
+    show(liveImageDivTemp)
+    hide(liveImageDiv)
+
+    liveImageDiv.replaceChildren(image(url))
+
+    await sleep(10)
+
+    hide(liveImageDivTemp)
+    show(liveImageDiv)
+
+    liveImageDivTemp.replaceChildren(image(url))
+})
+
+updateImg()
+setInterval(updateImg, 1500)
 
 function cum() {
     xhr.open('POST', APP_URL + 'notify')
